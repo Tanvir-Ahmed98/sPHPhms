3389ed4a604db4cf8cc41a3d0e3cd45abb366bb9
Tanvir Ahmed <55954927+Tanvir-Ahmed98@users.noreply.github.com>
2021-11-30 11:47:12 +0600

    home updated

--- script/home.php
+++ script/home.php
@@ -1,21 +1,87 @@
 <?php
 namespace sPHP;
-?>
+//upadted home page
+if($User->UserGroupIdentifierHighest() == "ADMINISTRATOR"){
 
-
-<center><div class="alert alert-success"> <strong> Visitor Page</strong></div></center>
-<center>
-<div class="container">
-  <div class="row">
-    <div class="col-sm-6">
-    <button>Appointment<span><img src="<?="{$Environment->ImageURL()}{$_SERVER["SERVER_NAME"]}/"?>appointment.png" style="height:150px;width:150px"></span></button><br>
-    </div>
-    <div class="col-sm-6">
-    <button>Find Doctors<span> <img src="<?="{$Environment->ImageURL()}{$_SERVER["SERVER_NAME"]}/"?>doctor.png" style="height:150px;width:150px"></span></button><br>
+  print '<center><div class="alert alert-success"> <strong> Admimistrator Page</strong></div></center>
+  <center>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-6">
+       
+      </div>
+      <div class="col-sm-6">
+     
+      </div>
     </div>
   </div>
-</div>
-</center>
+  </center>';
+  
+
+}else
+  
+if($User->UserGroupIdentifierHighest() == "PATIENT"){
+
+  print '<center><div class="alert alert-success"> <strong> Patient Page</strong></div></center>
+  <center>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-6">
+      </div>
+      <div class="col-sm-6">
+      <button class="btn btn-primary">Find Doctors</button><br>
+      </div>
+    </div>
+  </div>
+  </center>';
+
+
+
+}else
+
+
+if($User->UserGroupIdentifierHighest() == "DOCTOR"){
+
+  print '<center><div class="alert alert-success"> <strong> Doctor Page</strong></div></center>
+  <center>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-6">
+    
+      </div>
+      <div class="col-sm-6">
+     
+      </div>
+    </div>
+  </div>
+  </center>';
+
+
+
+}else
+
+
+
+print'<center><div class="alert alert-success"> <strong> This is homePage</strong></div></center>
+  <center>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-6">
+    
+      </div>
+      <div class="col-sm-6">
+     
+      </div>
+    </div>
+  </div>
+  </center>';
+
+
+
+
+
+
+?>
 
 
 
