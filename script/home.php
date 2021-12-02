2b77b14231a6562dffb57d9d4937b72b3c82e4e5
unknown <tanvir@bondstein.com>
2021-12-02 14:17:05 +0600

    home page updated

--- script/home.php
+++ script/home.php
@@ -3,13 +3,13 @@
 namespace sPHP;
 
 if ($User->UserGroupIdentifierHighest() == "ADMINISTRATOR") {
-  print '<center><div class="alert alert-success"><strong>Admimistrator page</strong></div></center>';
+  print '<center><div class="alert alert-success"><strong>Admimistrator home page</strong></div></center>';
 } else 
 if ($User->UserGroupIdentifierHighest() == "PATIENT") {
-  print '<center><div class="alert alert-success"> <strong>Patient page</strong></div></center>';
+  print '<center><div class="alert alert-success"> <strong>Patient home page</strong></div></center>';
 } else
 if ($User->UserGroupIdentifierHighest() == "DOCTOR") {
-  print '<center><div class="alert alert-success"> <strong>Doctor page</strong></div></center>';
+  print '<center><div class="alert alert-success"> <strong>Doctor home page</strong></div></center>';
 } else
   print '<center><div class="alert alert-success"> <strong>This is your home page</strong></div></center>';
 
